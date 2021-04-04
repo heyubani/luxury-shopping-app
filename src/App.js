90b2899b051d535038dc894c8d6a566d6f1e2dfc
Miracle <liegeubani@gmail.com>
2021-04-04 23:41:35 +0100

    created product list, updated app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import  React from "react";
+import React from "react";
+import Products from "./components/products";
+import data from "./data.json"
 
 //css component
 import "./App.css"
 
 
 class App extends React.Component {
+  state = {
+    products: data.products,
+    size:'',
+    sort:''
+  }
 
   render() {
+    console.log({Products})
   return (
     <div className="grid-container">
       <header>
-        <a href="http://$">nugget shopping chart</a>
+        <a href="http://$">Nugget shopping chart</a>
       </header>
-      <main>Product list </main>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products} /> 
+          </div>
+          <div className="cartBar">cart</div>
+        </div>  
+      </main>
       <footer>All right reserved.</footer>
     </div>
   );
