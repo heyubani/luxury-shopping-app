99092a3fdcbca4894614e36d8c0fe4a641e42674
Miracle <liegeubani@gmail.com>
2021-04-07 02:05:32 +0100

    add filtered component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,76 @@
-import React from "react";
-import Products from "./components/products";
-import data from "./data.json"
+import React from 'react';
+import data from './data.json';
+import Products from './components/products';
+import Filter from './components/filter';
 
 //css component
-import "./App.css"
-
+import './App.css';
 
 class App extends React.Component {
-  state = {
-    products: data.products,
-    size:'',
-    sort:''
-  }
+	state = {
+		products: data.products,
+		size: '',
+		sort: ''
+	};
 
-  render() {
-    console.log({Products})
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="http://$">Nugget shopping chart</a>
-      </header>
-      <main>
-        <div className="content">
-          <div className="main">
-            <Products products={this.state.products} /> 
-          </div>
-          <div className="cartBar">cart</div>
-        </div>  
-      </main>
-      <footer>All right reserved.</footer>
-    </div>
-  );
-  }
+	sortProduct = (e)  => {
+    const sort = e.target.value
+		console.log(e.target.value);
+    this.setState({
+      sort:sort,
+      products: this.state.products.slice().sort((a,b) => 
+        sort === "lowest" 
+        ? a.price > b.price
+        ? 1 
+        :-1 
+        : sort === "highest" 
+        ? a.price <  b.price
+        ? 1 
+        :-1
+        : a.price > b.price
+        ? 1 
+        :-1
+      )
+    })
+	}
+
+	filterProduct = (e) => {
+		console.log(e.target.value);
+		if (e.target.value === '') {
+			this.setState({ size: e.target.value, products: data.products });
+		} else {
+			this.setState({
+				size: e.target.value,
+				products: data.products.filter((product) => product.availableSizes.indexOf(e.target.value) >=0)
+			});
+		}
+	};
+
+	render() {
+		return (
+			<div className="grid-container">
+				<header>
+					<a href="http://$">Nugget shopping chart</a>
+				</header>
+				<main>
+					<div className="content">
+						<div className="main">
+							<Filter
+								count={this.state.products.length}
+								sort={this.state.sort}
+								size={this.state.size}
+								sortProduct={this.sortProduct}
+								filterProduct={this.filterProduct}
+							/>
+							<Products products={this.state.products} />
+						</div>
+						<div className="cartBar">cart</div>
+					</div>
+				</main>
+				<footer>All right reserved.</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
